1a64f658138f0f0010c3915379123baed6fb6804
itsonyeji <iqkeki@gmail.com>
2023-07-17 18:33:11 +0900

    브랜치에서 업로드5555

--- src/main/java/com/example/demo/Jin.java
+++ src/main/java/com/example/demo/Jin.java
@@ -4,9 +4,13 @@ package com.example.demo;
 
 왜
 public class Jin {
+    이거는 위에요
+            예지
  String dkfjdkjfdkjf;
+    예지
  String dkfjdkjfdkjfdk;
     이거 ㅁ되나여???~!!~
     12345
-
+    예지
+이거는 아래
 }
