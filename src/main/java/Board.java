14d9acfbfcfac39c461d700348e49cc9c9091524
Moonsun23 <socoooool90@gmail.com>
2023-07-17 17:52:11 +0900

    보아라!!!

---
+++ src/main/java/Board.java
@@ -0,0 +1,18 @@
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Board {
+    private String bno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+}
